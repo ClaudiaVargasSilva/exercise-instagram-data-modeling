2025367763863e6d6d64c472a11796a841f28f15
ClaudiaVargasSilva <clau.vargas.silva@gmail.com>
2021-02-02 17:01:22 +0000

    version2

--- src/models.py
+++ src/models.py
@@ -1,14 +1,15 @@
-import os
-import sys
+import os 
+import sys 
+import enum
+
 from sqlalchemy import Column, ForeignKey, Integer, String, Enum
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
 
-# crear clases 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -16,18 +17,21 @@ class User(Base):
     firstname = Column(String(250))
     lastname = Column(String(250))
     email = Column(String(250))
-    phone = Column(String(30))
+
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
 
+
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
+    type = Column(Enum)
     url = Column(String(250))
     post_id = Column(Integer, ForeignKey('post.id'))
+
 
 class Comment(Base):
     __tablename__ = 'comment'
@@ -36,11 +40,12 @@ class Comment(Base):
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
 
+
 class Follower(Base):
     __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey("user.id"))
-    user_to_id = Column(Integer, ForeignKey("user.id"))
+    user_from_id = Column(Integer, ForeignKey("user.id"),primary_key=True)
+    user_to_id = Column(Integer, ForeignKey("user.id"),primary_key=True)
+
 
 
 ## Draw from SQLAlchemy base
